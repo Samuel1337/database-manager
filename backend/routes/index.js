31bf87102b72ab2f5fffd9c8fe309b29e36e4853
Samuel Martins <smbra33@gmail.com>
2023-03-16 23:31:02 -0700

    create validation feature

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// test connection to server
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 // setup and connect to api routes
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
