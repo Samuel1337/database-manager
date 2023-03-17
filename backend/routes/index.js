44cfe7c903705e17ab47619a998066e37d8ac0fe
Samuel Martins <smbra33@gmail.com>
2023-03-16 23:09:29 -0700

    set up cookies cycle

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// test connection to server
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+// setup and connect to api routes
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
 module.exports = router;
