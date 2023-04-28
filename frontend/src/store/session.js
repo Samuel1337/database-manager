156f84ac83ae639795bde474e9519c56d821772c
Samuel Martins <smbra33@gmail.com>
2023-04-27 21:03:16 -0700

    set up modal

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,21 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
@@ -36,6 +51,14 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
 
 const initialState = { user: null };
 
